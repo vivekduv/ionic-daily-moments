f8cf57e9e274937d5ef8187ff4aaa3cc051cf6f5
Mirko Nasato <mirko@uptoskill.com>
2020-05-10 16:20:04 +0100

    Navigate between pages using React Router

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,26 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        Go to <Link to="/settings">Settings</Link>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default HomePage;
