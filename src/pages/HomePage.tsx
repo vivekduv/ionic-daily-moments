1fe35be6f9d945d0f86e638ab9e4bd9195be0048
Mirko Nasato <mirko@uptoskill.com>
2020-05-10 16:57:18 +0100

    Require login before displaying any private page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -22,7 +22,7 @@ const HomePage: React.FC = () => {
         <IonList>
           {entries.map((entry) =>
             <IonItem button key={entry.id}
-              routerLink={`/entries/${entry.id}`}>
+              routerLink={`/my/entries/${entry.id}`}>
               {entry.title}
             </IonItem>
           )}
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -16,7 +16,7 @@ interface Props {
 
 const LoginPage: React.FC<Props> = ({ loggedIn, onLogin }) => {
   if (loggedIn) {
-    return <Redirect to="/entries" />;
+    return <Redirect to="/my/entries" />;
   }
   return (
     <IonPage>
