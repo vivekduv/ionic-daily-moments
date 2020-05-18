e0fd19437bf22047e1473dc64a006269e51b08ac
Mirko Nasato <mirko@uptoskill.com>
2020-05-18 13:33:29 +0100

    Keep separate data for each user

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,15 +8,18 @@ import {
   IonToolbar,
 } from '@ionic/react';
 import React, { useState, useEffect } from 'react';
+import { useAuth } from '../auth';
 import { firestore } from '../firebase';
 import { Entry, toEntry } from '../models';
 
 const HomePage: React.FC = () => {
+  const { userId } = useAuth();
   const [entries, setEntries] = useState<Entry[]>([]);
   useEffect(() => {
-    const entriesRef = firestore.collection('entries');
+    const entriesRef = firestore.collection('users').doc(userId)
+      .collection('entries');
     entriesRef.get().then(({ docs }) => setEntries(docs.map(toEntry)));
-  }, []);
+  }, [userId]);
   return (
     <IonPage>
       <IonHeader>
